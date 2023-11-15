fecbc794b37c66a6427fb7f0705bb8ade1fac604
Clerc--Beaufort <alexis.clercbeaufort@gmail.com>
2023-11-15 08:52:58 +0100

    fonctions principales créées

---
+++ kifli.py
@@ -0,0 +1,33 @@
+def gen_pochon(n):
+    pass
+
+
+def gen_cle_privee(n):
+    pass
+
+
+def gen_cle_publique(cle_privee):
+    pass
+
+
+def solve_pochon(pochon, c):
+    pass
+
+
+def chiffrer(m, cle_publique):
+    pass
+
+
+def dechiffrer(mc, cle_privee):
+    pass
+
+
+def test_chiffrement_dechiffrement(message, n):
+    pass
+
+
+def main():
+    test_chiffrement_dechiffrement("Hello World!", 1024)
+    test_chiffrement_dechiffrement("Hello World!", 512)
+    test_chiffrement_dechiffrement("Kifli", 1024)
+    test_chiffrement_dechiffrement("Kifli", 512)
