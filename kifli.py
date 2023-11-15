c4f1803f538c89cd853f12ede438bd614d7e10d5
Clerc--Beaufort <alexis.clercbeaufort@gmail.com>
2023-11-15 09:15:31 +0100

    Fonctions de test implémentées

--- kifli.py
+++ kifli.py
@@ -23,11 +23,25 @@ def dechiffrer(mc, cle_privee):
 
 
 def test_chiffrement_dechiffrement(message, n):
-    pass
+    print("Message: {}".format(message))
+    cle_privee = gen_cle_privee(n)
+    cle_publique = gen_cle_publique(cle_privee)
+    mc = chiffrer(message, cle_publique)
+    m = dechiffrer(mc, cle_privee)
+    print("Message chiffré: {}".format(mc))
+    print("Message déchiffré: {}".format(m))
+
+
+def text_to_binary(text):
+    return [int(i) for i in ''.join([bin(ord(i))[2:].zfill(8) for i in text])]
+
+
+def binary_to_text(binary):
+    return ''.join([chr(int(''.join(binary[i:i + 8]), 2)) for i in range(0, len(binary), 8)])
 
 
 def main():
-    test_chiffrement_dechiffrement("Hello World!", 1024)
-    test_chiffrement_dechiffrement("Hello World!", 512)
-    test_chiffrement_dechiffrement("Kifli", 1024)
-    test_chiffrement_dechiffrement("Kifli", 512)
+    msg1 = "Hello World!"
+    msg2 = "Kifli"
+    test_chiffrement_dechiffrement(msg1, text_to_binary(msg1))
+    test_chiffrement_dechiffrement(msg2, text_to_binary(msg2))
