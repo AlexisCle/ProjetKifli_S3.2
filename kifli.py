b2c61699c2d7b32b88ad6596cb731d5d8cb1ae3b
Clerc--Beaufort <alexis.clercbeaufort@gmail.com>
2023-12-01 19:35:54 +0100

    gen_cle_privee modifiée fonction dechiffrer corrigée nouvelle façon de permuter les tableaux intégrée

--- kifli.py
+++ kifli.py
@@ -14,11 +14,11 @@ def gen_pochon(n):
 
 def gen_cle_privee(n):
     pochon = gen_pochon(n)
-    m = sum(pochon) + random.randint(1, 1000)
+    m = sum(pochon) + random.randint(1, 100)
     w = random.randint(1, m - 1)
     while gcd(w, m) != 1:
         w = random.randint(1, m - 1)
-    permutation_sigma = random.sample(range(1, n + 1), n)
+    permutation_sigma = random.sample(range(0, n), n)
     cle_privee = {'pochon': pochon, 'M': m, 'W': w, 'sigma': permutation_sigma}
     return cle_privee
 
@@ -29,14 +29,15 @@ def gen_cle_publique(cle_privee):
     m = cle_privee['M']
     w = cle_privee['W']
     permutation_sigma = cle_privee['sigma']
+    pochonsigma = permutation_tableau(pochon, permutation_sigma)
     for i in range(len(pochon)):
-        cle_publique.append((w * pochon[permutation_sigma[i] - 1]) % m)
+        cle_publique.append((w * pochonsigma[i]) % m)
     return cle_publique
 
 
 def solve_pochon(pochon, c):
     res = []
-    cnt =len(pochon)
+    cnt = len(pochon)
     while c >= 0 and cnt > 0:
         if c >= pochon[cnt-1]:
             res.append(1)
@@ -48,6 +49,7 @@ def solve_pochon(pochon, c):
 
 
 def chiffrer(m, cle_publique):
+    print(m)
     res = 0
     for i in range(len(m)):
         res += m[i] * cle_publique[i]
@@ -55,20 +57,22 @@ def chiffrer(m, cle_publique):
 
 
 def dechiffrer(message_chiffre, cle_privee):
-    message_original = []
+    #message_original = [0] * len(cle_privee['pochon'])
     pochon = cle_privee['pochon']
     m = cle_privee['M']
     w = cle_privee['W']
     permutation_sigma = cle_privee['sigma']
-    d = (w**-1 * message_chiffre) % m
+    inverse_mod_w = pow(w, -1, m)
+    d = (inverse_mod_w * message_chiffre) % m
     bits_message_original = solve_pochon(pochon, d)
-    for i in range(len(bits_message_original)):
-        message_original.append(bits_message_original[permutation_sigma.index(i + 1)])
+    print(bits_message_original)
+    message_original = inverse_permutation_tableau(bits_message_original, permutation_sigma)
     return message_original
 
 
-def test_chiffrement_dechiffrement(message, n):
+def test_chiffrement_dechiffrement(message):
     print("Message:           {}".format(message))
+    n = len(message)
     cle_privee = gen_cle_privee(n)
     cle_publique = gen_cle_publique(cle_privee)
     mc = chiffrer(message, cle_publique)
@@ -82,13 +86,29 @@ def text_to_binary(text):
     return [int(i) for i in ''.join([bin(ord(i))[2:].zfill(8) for i in text])]
 
 
-# Turns a binary list into a string
 def binary_to_text(binary):
     return ''.join([chr(int(''.join([str(binary[i+j]) for j in range(8)]), 2)) for i in range(0, len(binary), 8)])
+
+
+def permutation_tableau(tableau_original, tableau_permutation):
+    tableau_permute = [0] * len(tableau_original)
+    for i in range(len(tableau_permutation)):
+        indice_permutation = tableau_permutation[i]
+        tableau_permute[i] = tableau_original[indice_permutation]
+    return tableau_permute
+
+
+def inverse_permutation_tableau(tableau_permute, tableau_permutation):
+    tableau_original = [0] * len(tableau_permute)
+    for i in range(len(tableau_permutation)):
+        indice_permutation = tableau_permutation[i]
+        tableau_original[indice_permutation] = tableau_permute[i]
+    return tableau_original
 
 
 def main():
     msg1 = "Hello World!"
     msg2 = "Kifli"
+    binmsg = [1, 0, 1, 0, 1]
     #test_chiffrement_dechiffrement(text_to_binary(msg1), len(msg1) * 8)
-    test_chiffrement_dechiffrement(text_to_binary(msg2), len(msg2) * 8)
+    test_chiffrement_dechiffrement(text_to_binary(msg2))
